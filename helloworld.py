74d17b7bc5503af25364588036404849bde130f3
Josh Peterson <joshuadanpeterson@gmail.com>
2023-07-22 20:26:50 -0600

    Merge conflict

--- helloworld.py
+++ helloworld.py
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
 def hello_world(city, state):
     print(f'Hello World {city}, {state}!')
 
 
 def main():
     hello_world('NYC', 'New York')
-=======
-def hello_world(city):
-    print(f'Hello World {city}!')
-
-
-def main():
-    hello_world('NYC')
->>>>>>> 6883eae744fa2fbf927da8d16fb23cbe3329029c
 
 
 main()
